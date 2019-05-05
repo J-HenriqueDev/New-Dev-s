e77cee5891c1e6484a56a8900563a06370fcd07f
Henrique <45069999+Henriqueaccount@users.noreply.github.com>
2019-05-05 11:23:36 -0300

    Update errors.py

--- cogs/errors.py
+++ cogs/errors.py
@@ -5,7 +5,7 @@ class errors(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, discord.ext.commands.errors.CommandNotFound):
             pass
