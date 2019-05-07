4f1be44c89244d12c2ee1378e48eb4992c27f45e
Henrique <45069999+Henriqueaccount@users.noreply.github.com>
2019-05-06 22:52:04 -0300

    Update helper.py

--- cogs/desativado/helper.py
+++ cogs/desativado/helper.py
@@ -304,17 +304,17 @@ class helper(commands.Cog):
                                 try:                                    
                                     q = await user.send(f" | **Digite o motivo pelo qual você está recusando o Helper `{ctx.author}`**. **`(5 minutos)`**")                                    
                                 def check(m):
-                                   return m.channel.id == q.channel.id and m.author.id == user_id
+                                    return m.channel.id == q.channel.id and m.author.id == user_id
 
                                 try:
                                     motivo = await self.lab.wait_for("message", check=check, timeout=300)
-                                   embed.add_field(name=":bell: Motivo", value = "``"+str(message.content)+"``", inline=True)
-                                   embed.set_thumbnail(url=ctx.author.avatar_url_as())
-                                   embed.set_footer(text=self.bard.user.name+" © 2019", icon_url=self.bard.user.avatar_url_as())
-                                   server = self.bard.get_guild(570906068277002271)
+                                embed.add_field(name=":bell: Motivo", value = "``"+str(message.content)+"``", inline=True)
+                                embed.set_thumbnail(url=ctx.author.avatar_url_as())
+                                embed.set_footer(text=self.bard.user.name+" © 2019", icon_url=self.bard.user.avatar_url_as())
+                                server = self.bard.get_guild(570906068277002271)
                                       #canal solicitação
-                                   channel = discord.utils.get(server.channels, id=571087828482523146)
-                                   await channel.send(embed=embed)
+                                channel = discord.utils.get(server.channels, id=571087828482523146)
+                                await channel.send(embed=embed)
          except asyncio.TimeoutError:
              self.forms.remove(ctx.author.id)             
              await msg.delete()
