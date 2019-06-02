5224f9c985236f61c4cd1169dfeb206ad3f29802
Henrique <henriquevc8899@gmail.com>
2019-06-02 13:46:41 -0300

    Update rep.py

--- cogs/rep.py
+++ cogs/rep.py
@@ -16,88 +16,97 @@ timetime=dict()
 
 
 class rep(commands.Cog):
-    def __init__(self, bard):
-        self.bard = bard
-        self.canais = ["570908357032935425","571014988622331905"]
+   def __init__(self, bard):
+      self.bard = bard
+      self.canais = ["570908357032935425","571014988622331905"]
     
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.cooldown(1,10,commands.BucketType.user)
-    @commands.guild_only()
-    @commands.command(description='Dê um ponto de reputação para quando um </New Helper> lhe ajudar.',usage='c.rep @</New Helper>')
-    async def rep(self, ctx, *, user: discord.Member = None):
-           if not str(ctx.channel.id) in self.bard.canais and not str(ctx.message.author.id) in self.bard.staff:
-               await ctx.message.add_reaction(":incorreto:571040727643979782")
+   @commands.bot_has_permissions(embed_links=True)
+   @commands.cooldown(1,10,commands.BucketType.user)
+   @commands.guild_only()
+   @commands.command(description='Dê um ponto de reputação para quando um </New Helper> lhe ajudar.',usage='c.rep @</New Helper>')
+   async def rep(self, ctx, *, user: discord.Member = None):
+         if not str(ctx.channel.id) in self.bard.canais and not str(ctx.message.author.id) in self.bard.staff:
+            await ctx.message.add_reaction(":incorreto:571040727643979782")
+            return
+         if user is None:
+            embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, mencione o usuário que você gostaria de dá a reputação.", color=0x7289DA)
+            msg = await ctx.send(embed=embed)
+            await asyncio.sleep(20)
+            await msg.delete()              
+            return
+         else:
+            usuario = user
+            if usuario.bot is True:
+               embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, não é possível dá reputação ao um **BOT**.", color=0x7289DA)
+               msg = await ctx.send(embed=embed)
+               await asyncio.sleep(20)
+               await msg.delete()              
                return
-           if user is None:
-              embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, mencione o usuário que você gostaria de dá a reputação.", color=0x7289DA)
-              msg = await ctx.send(embed=embed)
-              await asyncio.sleep(20)
-              await msg.delete()              
-              return
-           else:
-             usuario = user
-             if usuario.bot is True:
-                embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, não é possível dá reputação ao um **BOT**.", color=0x7289DA)
-                msg = await ctx.send(embed=embed)
-                await asyncio.sleep(20)
-                await msg.delete()              
-                return
-             if not str("</NewHelper>") in [r.name for r in user.roles if r.name != "@everyone"]:
+            if not str("</NewHelper>") in [r.name for r in user.roles if r.name != "@everyone"]:
                 embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, o usuário {user.mention} não é um **</NewHelper>** registrado.", color=0x7289DA)
                 msg = await ctx.send(embed=embed)
                 await asyncio.sleep(20)
                 await msg.delete()              
                 return
-             if ctx.author.id == user.id:
-                embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, não é possível dá reputação a si mesmo.", color=0x7289DA)
-                msg = await ctx.send(embed=embed)
-                await asyncio.sleep(20)
-                await msg.delete()              
-                return
+            if ctx.author.id == user.id:
+               embed=discord.Embed(description=f"<:incorreto:571040727643979782> **|** Olá **{ctx.author.name}**, não é possível dá reputação a si mesmo.", color=0x7289DA)
+               msg = await ctx.send(embed=embed)
+               await asyncio.sleep(20)
+               await msg.delete()              
+               return
              
-             if ctx.author.id in timetime:
-                w = json.loads(timetime[ctx.author.id])
-                if time.time() < w:
-                   w = int(w) - int(time.time())
-                   minute = 60
-                   hour = minute * 60
-                   day = hour * 24
-                   days =  int(w / day)
-                   hours = int((w % day) / hour)
-                   minutes = int((w % hour) / minute)
-                   seconds = int(w % minute)
-                   string = ""
-                   if days > 0:
-                      string += str(days) + " " + (days == 1 and "dia" or "dias" ) + ", "
-                   if len(string) > 0 or hours > 0:
-                      string += str(hours) + " " + (hours == 1 and "hora" or "horas" ) + ", "
-                   if len(string) > 0 or minutes > 0:
-                      string += str(minutes) + " " + (minutes == 1 and "minuto" or "minutos" ) + ", "
-                   string += str(seconds) + " " + (seconds == 1 and "segundo" or "segundos" )
-                   embed=discord.Embed(description=f"<:timer:565975875988750336> **|** Olá **{ctx.author.name}**, você precisa esperar **{str(string)}** para da uma nova reputação ao usuário.", color=0x7289DA)
-                   await ctx.send(embed=embed)
-                   return
-             mongo = MongoClient(self.bard.database)
-             bard = mongo['bard']
-             users = bard['users']
-             usuario = bard.users.find_one({'_id': str(usuario.id)})
-             if usuario is None:
-                embed=discord.Embed(description=f"<:correto:571040855918379008> **|** Olá **{ctx.author.name}**, você deu **1** ponto de reputação ao usuário {user.mention}.", color=0x7289DA)
-                await ctx.send(embed=embed)
-                rep = int(usuario["reputação"])+int(1)
-                bard.users.update_one({'_id': str(user.id)}, {'$set': {'reputação':int(rep)}})
-             else:
+            if ctx.author.id in timetime:
+               w = json.loads(timetime[ctx.author.id])
+               if time.time() < w:
+                  w = int(w) - int(time.time())
+                  minute = 60
+                  hour = minute * 60
+                  day = hour * 24
+                  days =  int(w / day)
+                  hours = int((w % day) / hour)
+                  minutes = int((w % hour) / minute)
+                  seconds = int(w % minute)
+                  string = ""
+                  if days > 0:
+                     string += str(days) + " " + (days == 1 and "dia" or "dias" ) + ", "
+                  if len(string) > 0 or hours > 0:
+                     string += str(hours) + " " + (hours == 1 and "hora" or "horas" ) + ", "
+                  if len(string) > 0 or minutes > 0:
+                     string += str(minutes) + " " + (minutes == 1 and "minuto" or "minutos" ) + ", "
+                  string += str(seconds) + " " + (seconds == 1 and "segundo" or "segundos" )
+                  embed=discord.Embed(description=f"<:timer:565975875988750336> **|** Olá **{ctx.author.name}**, você precisa esperar **{str(string)}** para da uma nova reputação ao usuário.", color=0x7289DA)
+                  await ctx.send(embed=embed)
+                  return
+            mongo = MongoClient(self.bard.database)
+            bard = mongo['bard']
+            users = bard['users']
+            usuario = bard.users.find_one({'_id': str(usuario.id)})
+            if usuario is None:
+               embed=discord.Embed(description=f"<:correto:571040855918379008> **|** Olá **{ctx.author.name}**, você deu **1** ponto de reputação ao usuário {user.mention}.", color=0x7289DA)
+               await ctx.send(embed=embed)
+               rep = int(usuario["reputação"])+int(1)
+               bard.users.update_one({'_id': str(user.id)}, {'$set': {'reputação':int(rep)}})
+            else:
                embed=discord.Embed(description=f"<:correto:571040855918379008> **|** Olá **{ctx.author.name}**, você deu **1** de reputação ao usuário {user.mention}.", color=0x7289DA)
                await ctx.send(embed=embed)
                rep = int(usuario["reputação"])+int(1)
                bard.users.update_one({'_id': str(user.id)}, {'$set': {'reputação':int(rep)}})
 
-    @commands.command(
+   @rep.error
+   async def rep_error(self, ctx, error):
+      if isinstance(error, commands.BadArgument):
+         comma = error.args[0].split('"')[1]
+         embed = discord.Embed(title=f"{self.bard._emojis['incorreto']} | Membro não encontrado!", color=0x7289DA, description=f"O membro `{comma}` não está nesse servidor.")
+         await ctx.send(embed=embed)
+         return
+       
+
+   @commands.command(
       name='delrep',
       description='Reseta os pontos mensais de reputação de todos os helpers',
       usage='c.delrep'
     )
-    async def _resetarreps(self, ctx):
+   async def _resetarreps(self, ctx):
        if not ctx.author.id in self.bard.dono:
             await ctx.send(
                 f"<:errado:567782857863593995>{ctx.author.mention} você não é um administrador para utilizar esse comando.",
@@ -110,8 +119,8 @@ class rep(commands.Cog):
        await ctx.send(f"<:correto:571040855918379008> | **{ctx.author.name}**, a reputação dos NewHelpers foram resetadas.")
 
 
-    @commands.command()
-    async def reps(self, ctx, membro: discord.Member = None):
+   @commands.command()
+   async def reps(self, ctx, membro: discord.Member = None):
          if membro is None:
             usuario = ctx.author
             titulo = f"Olá {usuario.name}, veja a sua quantidade de rep's a baixo."
